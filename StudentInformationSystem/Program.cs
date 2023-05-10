f954ad3deaea2231137da7804e17b7886ee67b35
Stephen Perez <perezstephenmathew360@gmail.com>
2023-05-10 08:57:48 +0800

    Added some stuff

--- StudentInformationSystem/Program.cs
+++ StudentInformationSystem/Program.cs
@@ -4,12 +4,9 @@ class Program
     static void Main(string[] args)
     {
         //this will be our main program, create your designated class to create function on your code
-        DisplayLayout dl = new DisplayLayout();
-        dl.mainDisplay();
-        dl.userInput();
+        CommandFunction.userLogin();
+        CommandFunction.actionMainDisplay();
 
-        CommandFunction cf = new CommandFunction();
-        cf.askLogout();
     }
 }
 
