5fbec1fee768e6d00b18627aa4e597ccd3843011
Stephen Perez <perezstephenmathew360@gmail.com>
2023-06-19 14:06:22 +0800

    Added some stuff

---
+++ SIS_RULES/StudentFormRules.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using SIS_UI;
+using SIS_MODEL;
+using SIS_DATA;
+using AM_Rules;
+
+namespace SIS_RULES
+{
+    public class StudentFormRules
+	{
+        public static void actionStudentWelcomePage()
+		{
+
+			int status;
+			int action;
+            do
+			{
+				status = StudentForm.studentWelcomePage();
+				switch (status)
+				{
+					case 0: break;
+					case 1: //student information
+                        PrintInfos.PrintStudentInfo(StudentForm.username);
+                        break;
+
+					case 2: //personal information
+						status = StudentForm.actionStudentPersonalInfo();
+						switch (status)
+						{
+							case 0: break;
+							case 1:
+								PrintInfos.PrintStudentPersonalInfo(StudentForm.username);
+								break;
+							case 2: break;
+						}
+						//PrintInfos.PrintStudentPersonalInfo(StudentForm.username);
+						break;
+
+					case 3: StudentForm.gradingSystem(); break;
+					case 4: StudentForm.attendance(); break;
+				}
+			} while (status == null);
+		}
+
+       
+
+
+    }
+}
+
---
+++ SIS_UI/StudentForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SIS_UI
+{
+	public class StudentForm
+	{
+        public static string username;
+
+        public static int studentWelcomePage()
+		{
+            Console.WriteLine("\nPlease choose an action: ");
+            Console.WriteLine("[0]Log out");
+            Console.WriteLine("[1]Student Information");
+            Console.WriteLine("[2]Personal Information");
+            Console.WriteLine("[3]Grading System");
+            Console.WriteLine("[4]Attendance");
+            Console.Write("Input: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static int actionStudentPersonalInfo()
+        {
+            Console.WriteLine("\nPlease choose an action: ");
+            Console.WriteLine("[0]Go Back");
+            Console.WriteLine("[1]View Student Personal Information");
+            Console.WriteLine("[2]Update Student Personal Information");
+            Console.Write("Input: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static void attendance()
+        {
+            Console.WriteLine("On progress of Nico's Group");
+        }
+
+        public static void gradingSystem()
+        {
+            Console.WriteLine("On Progress of Rizon's Group");
+        }
+
+        public static void studentInformation()
+        {
+
+        }
+    }
+}
+
